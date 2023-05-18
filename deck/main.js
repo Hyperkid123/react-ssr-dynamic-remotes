404f7acb93d678ae4a4e1286f8e20724a95542d7
Martin Maroši <marvusm.mmi@gmail.com>
2023-05-18 17:16:44 +0200

    Slide deck updates.

--- deck/main.js
+++ deck/main.js
@@ -1,14 +1,21 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/black.css'
+import 'highlight.js/styles/vs2015.css'
 import './style.css'
 
 let deck = new Reveal({
-   plugins: [ Markdown ]
+   
 })
 
-deck.initialize();
+deck.initialize({
+  highlight: {
+
+  },
+  plugins: [ Markdown, RevealHighlight]
+});
 
 deck.on( 'make-it-pop', () => {
   console.log('✨');
