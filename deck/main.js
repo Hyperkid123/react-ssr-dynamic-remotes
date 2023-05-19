a01ffc784d4290227cdcf62e65ad85528dbfbcb7
Martin Maroši <marvusm.mmi@gmail.com>
2023-05-19 15:07:48 +0200

    Add speaker notes plugin

--- deck/main.js
+++ deck/main.js
@@ -1,6 +1,7 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight'
+import Notes from 'reveal.js/plugin/notes/notes'
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/black.css'
 import 'highlight.js/styles/vs2015.css'
@@ -14,7 +15,7 @@ deck.initialize({
   highlight: {
 
   },
-  plugins: [ Markdown, RevealHighlight]
+  plugins: [ Markdown, RevealHighlight, Notes]
 });
 
 deck.on( 'make-it-pop', () => {
